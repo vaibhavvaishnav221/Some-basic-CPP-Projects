3de45a82ff3a7c2085ef4a72fc853c451d67c088
Vaibhav Vaishnav <vaibhavvaishnav221@gmail.com>
2025-01-04 18:25:43 +0530

    Update operators.cpp
    Remove unwanted lines of code

--- operators.cpp
+++ operators.cpp
@@ -11,11 +11,6 @@ int main()
     cout << "Enter a number : ";
     cin >> b;
 
-    cout << "Sum of " << a << " and " << b << " is " << a + b << "." << endl;
-    cout << "Difference of " << a << " and " << b << " is " << a - b << "." << endl;
-    cout << "Multiplicaton of " << a << " and " << b << " is " << a * b << "." << endl;
-    cout << "Division of " << a << " and " << b << " is " << a / b << "." << endl;
-
     cout << "SUM : " << a + b << endl;
     cout << "DIFFERENCE : " << a - b << endl;
     cout << "MULTIPLICATION : " << a * b << endl;
