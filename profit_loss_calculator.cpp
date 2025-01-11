7d734f3532e0db8a3af9219a5f4978e55ac8554b
Vaibhav Vaishnav <vaibhavvaishnav221@gmail.com>
2025-01-11 12:00:54 +0530

    Add files via upload

---
+++ profit_loss_calculator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sp;
+    cout << "Enter Selling Price: ";
+    cin >> sp;
+
+    int cp;
+    cout << "Enter Cost Price";
+    cin >> cp;
+
+    if (cp > sp)
+    {
+        cout << "Loss: " << cp - sp << endl;
+    }
+    else if (cp = sp)
+    {
+        cout << "No Profit/Loss" << endl;
+    }
+    else
+    {
+        cout << "Profit: " << sp - cp << endl;
+    }
+}
